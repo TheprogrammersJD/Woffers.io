ab8579cf9b49d11924356ad29f25545c4a9ca7aa
TheprogrammersJD <solarisfree101@gmail.com>
2018-08-03 16:35:06 -0500

    Added mobile support

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ var rooms = [];
 var roomnum = 0;
 var playersonline = 0;
 var roomlimit = 3;//The number of people allowed in each room
+var roomwidth = 1000;
+var roomheight = 1000;
 
 io.on('connection', function(socket){
 	console.log('User ' + socket.id + ' connected.');
@@ -28,6 +30,7 @@ io.on('connection', function(socket){
 	socket.broadcast.emit('player', players[playernum]);
 
 	io.to(socket.id).emit('youare', players[socketids.indexOf(socket.id)]);
+	io.to(socket.id).emit('roomsize', roomwidth, roomheight);
 	io.to(socket.id).emit('playersonline', playersonline);
 	playernum++;
 	playersonline++;
@@ -39,8 +42,10 @@ io.on('connection', function(socket){
 	socket.on('ready', function(id){
 		for(var p = 0; p < players.length; p++){
 			if(players[p].id == id){
+				players[p].ready = true;
 				if(rooms.length < 1){ //If there are no rooms
 					socket.join('room'+roomnum);//
+					io.to(socket.id).emit('yourroom', roomnum);
 					players[p].room = roomnum;//
 					rooms.push(1);//
 					roomnum++;//
@@ -50,14 +55,16 @@ io.on('connection', function(socket){
 					for(var r = 0; r < rooms.length; r++){
 						if(rooms[r] < roomlimit){//If there are fewer than 3 people in this room, join it
 							socket.join('room'+r);
+							io.to(socket.id).emit('yourroom', r);
 							players[p].room = (r);
 							rooms[r]++;
 							console.log(rooms);
 							r = rooms.length;//And end the loop so you don't join more than one room =P
 						}
-						else if(rooms[r] >= roomlimit){//If the number of people in this room is greater than or equal to 3
+						else if(rooms[r] >= roomlimit){//If the number of people in this room is greater than or equal to the rooms limit
 							if(r == rooms.length - 1){//And we've checked all the rooms for an opening
 								socket.join('room'+roomnum);//Make a new room and join it
+								io.to(socket.id).emit('yourroom', roomnum);
 								players[p].room = roomnum;
 								rooms.push(1);
 								roomnum++;
@@ -67,10 +74,55 @@ io.on('connection', function(socket){
 						}
 					}
 				}
+				
+				for(var pir = 0; pir < players.length; pir++){
+					if(players[pir].room == players[p].room&&players[pir].online == true&&players[p].room != null){
+						if(players[pir].id != socket.id){
+							io.to(socket.id).emit('playersinroom', players[pir]);//Tell yourself who else is in this room
+							io.to(players[pir].id).emit('playersinroom', players[p]);//Tell everyone else you joined this room
+						}
+					}
+				}
 			}
 			else{}
+		}	
+	});
+	
+	socket.on('mrqst', function(angle, quad){
+		for(var mp = 0; mp < players.length; mp++){
+			if(players[mp].alive == true&&players[mp].id == socket.id){//&&players[mp].x<1000&&players[mp].y<1000&&players[mp].x>0&&players[mp].y>0
+				//io.to(players[mp].room).emit('move', socket.id, players[mp].x, players[mp].y);
+				//socket.broadcast.emit('move', socket.id, players[mp].x, players[mp].y);
+				//io.to(socket.id).emit('move', socket.id, players[mp].x, players[mp].y);
+				if(angle < 0&&quad == 1&&players[mp].x<roomwidth){
+					players[mp].x -= (Math.sin(angle));
+				}
+				if(angle < 0&&quad == 1&&players[mp].y>0){
+					players[mp].y -= (Math.cos(angle));
+				}
+				if(angle > 0&&quad == 2&&players[mp].x>0){
+					players[mp].x -= (Math.sin(angle));
+				}
+				if(angle > 0&&quad == 2&&players[mp].y>0){
+					players[mp].y -= (Math.cos(angle));
+				}
+				if(angle < 0&&quad == 3&&players[mp].x>0){
+					players[mp].x += (Math.sin(angle));
+				}
+				if(angle < 0&&quad == 3&&players[mp].y<roomheight){
+					players[mp].y += (Math.cos(angle));
+				}
+				if(angle > 0&&quad == 4&&players[mp].x<roomwidth){
+					players[mp].x += (Math.sin(angle));
+				}
+				if(angle > 0&&quad == 4&&players[mp].y<roomheight){
+					players[mp].y += (Math.cos(angle));
+				}
+				
+				socket.emit('move', socket.id, players[mp].x, players[mp].y);
+				io.to('room' + players[mp].room).emit('move', socket.id, players[mp].x, players[mp].y);
+			}
 		}
-		//socket.broadcast.emit('click', x, y);
 	});
 	
 	socket.on('disconnect', function(){
@@ -101,13 +153,15 @@ function getRandomArbitrary(min, max) {
 function player(id){
 	this.name = adjectives[(Math.ceil(Math.random()*adjectives.length))-1]+"-"+nouns[(Math.ceil(Math.random()*nouns.length))-1]+"-"+(Math.ceil(Math.random()*1000));
 	this.id = id;
-	this.x = 0;
-	this.y = 0;
+	this.x = getRandomArbitrary(0, roomwidth);
+	this.y = getRandomArbitrary(0, roomwidth);
 	this.score = 0;
+	this.ready = false;
 	this.alive = true;
 	this.online = true;
 	this.room = null;
 	this.attacking = false;
+	this.attackrechargetimer = 180;
 	players.push(this);
 	socketids.push(this.id);
 }
